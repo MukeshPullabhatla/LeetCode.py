dc8b43fd8b3e92198e1a130a1b35b62281b13583
MukeshPullabhatla <mpullabh@uncc.edu>
2021-07-26 12:29:43 -0400

    first Commit

---
+++ LeetCode97.py
@@ -0,0 +1,35 @@
+'''
+97. Interleaving String
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+Example 1:
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Example 3:
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+Constraints:
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lowercase English letters.
+'''
+class Solution:
+    @lru_cache(None)
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if not s1 and not s2 and not s3: return True
+        if not s3: return False
+        if s1 and s2 and s3[-1] == s2[-1] and s3[-1] == s1[-1]:
+            return self.isInterleave(s1, s2[:-1], s3[:-1]) or self.isInterleave(s1[:-1],s2,s3[:-1])
+        if s2 and s3[-1] == s2[-1]:
+            return self.isInterleave(s1, s2[:-1],s3[:-1])
+        if s1 and s3[-1] == s1[-1]:
+            return self.isInterleave(s1[:-1],s2,s3[:-1])
+        return False
